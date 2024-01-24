7359ac99d79c4c533ea3c3dc3eea17e6f547ff7e
tammeusb <tammeusb@gmail.com>
2024-01-24 14:42:24 -0500

    add pages and show_image function

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const rockSelect = document.querySelector(".rock"); 
-const rockMessage = document.querySelector(".rockMessage")
+const message = document.querySelector(".message")
 
 rockSelect.addEventListener("change", setRock);
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    message.appendChild(img);
+}
 
 function setRock() {
     const rockChoice = rockSelect.value;
 
     if (rockChoice === "sodalite") {
-        rockMessage.textContent = 
+        message.textContent = 
         "You feel like a whole being whose air is dispersed evenly.";
+        show_image('./images/rocks/sodalite.jpg', 200, 200, 'Sodalite');
     } else {
-        rockMessage.textContent =
+        message.textContent =
         "";
     }
 }
