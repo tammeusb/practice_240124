a373245fc7c145a55eb6c474093791a058c16f30
tammeusb <tammeusb@gmail.com>
2024-01-24 15:14:39 -0500

    add white tea option and rename variables

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const rockSelect = document.querySelector(".rock"); 
+const choice = document.querySelector(".choice"); 
 const message = document.querySelector(".message")
 
-rockSelect.addEventListener("change", setRock);
+choice.addEventListener("change", setChoice);
 
 function show_image(src, width, height, alt) {
     var img = document.createElement("img");
@@ -13,15 +13,37 @@ function show_image(src, width, height, alt) {
     message.appendChild(img);
 }
 
-function setRock() {
-    const rockChoice = rockSelect.value;
+function setChoice() {
+    const option = choice.value;
 
-    if (rockChoice === "sodalite") {
+    //rocks options
+
+    if (option === "sodalite") {
         message.textContent = 
-        "You feel like a whole being whose air is dispersed evenly.";
+        "You feel like a whole being whose air has dispersed evenly...";
         show_image('./images/rocks/sodalite.jpg', 200, 200, 'Sodalite');
+    } else if (option === "quartz") {
+        message.textContent =
+        "You begin to know a clarity like your old backyard pool...";
+        show_image('./images/rocks/quartz.jpg', 200, 200, 'Quartz');
+    } else if (option === "desert-rose") {
+        message.textContent =
+        "You sense the fruit of intuition ripe behind your eyes...";
+        show_image('./images/rocks/desert-rose.jpg', 250, 200, 'Desert Rose');
+    } else if (option === "tourmaline") {
+        message.textContent =
+        "You feel your inner tension's tide subtly change direction...";
+        show_image('./images/rocks/tourmaline.jpg', 250, 200, 'Tourmaline');
+        
+    //teas options
+
+    } else if (option === "white") {
+        message.textContent = 
+        "You feel like the last unpicked flower drying in the comfy sun...";
+        show_image('../images/teas/white.jpg', 200, 200, 'White Tea');
     } else {
         message.textContent =
         "";
     }
+
 }
