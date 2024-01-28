8b04cd67490cdfe9af82c62187cb21e4ab9f8725
tammeusb <tammeusb@gmail.com>
2024-01-27 21:36:03 -0500

    add mergeHertz function with one basic output

--- pages/freq.js
+++ pages/freq.js
@@ -2,9 +2,14 @@ const hertzInputOne = document.querySelector(".freq-one");
 const hertzInputTwo = document.querySelector(".freq-two");
 const hertzInputThree = document.querySelector(".freq-three");
 
+const button = document.querySelector(".merge");
+
 const log = document.querySelector(".log");
 
-hertzInputThree.addEventListener("input", mergeHertz);
+button.addEventListener("click", mergeHertz);
+
+// hertzInputThree.addEventListener("input", mergeHertz); 
+// save this event listener to change bg color of fields? 
 
 function mergeHertz() {
 
@@ -12,5 +17,15 @@ function mergeHertz() {
     const hertzValueTwo = hertzInputTwo.value;
     const hertzValueThree = hertzInputThree.value;
 
-    log.textContent = hertzValueOne + " " + hertzValueTwo + " " + hertzValueThree;
+    const output = document.querySelector(".output")
+
+    //debug
+    // log.textContent = hertzValueOne + " " + hertzValueTwo + " " + hertzValueThree;
+
+    if (hertzValueOne < 50 && (hertzValueTwo > 50 && hertzValueTwo < 100) && hertzValueThree >= 100 ) {
+        output.textContent = "You've made a nice spacious chord.";
+    } else {
+        output.textContent = "";
+    }
+
 }
